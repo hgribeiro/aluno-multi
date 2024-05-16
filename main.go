0025a1a92f53e4b8fba11cf5547fc45e54e67331
hgribeiro <hgribeiro@live.com>
2024-05-15 22:11:17 -0300

    basic api http server

--- main.go
+++ main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"net/http"
 )
 
-func worker(workerNum int, data chan int) {
-	for msg := range data {
-		fmt.Printf("Worker %d: %d\n", workerNum, msg)
-		time.Sleep(time.Second)
-	}
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/users", listUserHandler)
+
+	http.ListenAndServe(":8080", mux)
+
 }
 
-func main() {
-	canal := make(chan int)
-
-	qtWorkers := 50
-
-	// EU PRECISO INICIAR A GO ROUTINE ANTES DE ENVIAR PARA ELE, SE NAO DA DEADLOCK.
-	go worker(1, canal)
-	go worker(2, canal)
-
-	for i := 0; i < qtWorkers; i++ {
-		canal <- i
-	}
-
-	close(canal) // Feche o canal depois de enviar todos os dados.
-
+func listUserHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("List User"))
 }
